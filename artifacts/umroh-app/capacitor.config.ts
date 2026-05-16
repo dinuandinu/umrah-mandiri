28d59c21a85a0a14f2f8da7c91df24c3213ddd7b
dinuandinu <dinu.andinu@gmail.com>
2026-05-16 14:55:10 +0700

    fix: update webDir to dist/public

--- artifacts/umroh-app/capacitor.config.ts
+++ artifacts/umroh-app/capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.dinuandinu.umrahmandiri',
   appName: 'Umroh Mandiri',
-  webDir: 'dist',
+  webDir: 'dist/public',
   server: {
     androidScheme: 'https'
   }
