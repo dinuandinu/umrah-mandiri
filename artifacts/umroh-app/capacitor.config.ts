d7bce8f23f588ff3062d375e64c88c6d0367e658
dinuandinu <76004768+dinuandinu@users.noreply.github.com>
2026-05-19 12:15:27 +0700

    Restore capacitor.config.ts

---
+++ artifacts/umroh-app/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.dinuandinu.umrahmandiri',
+  appName: 'Umroh Mandiri',
+  webDir: 'dist/public',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
